b06f9318dfc701dc03f16afd65d33b65ad3f7627
Kun Qin <kuqin@microsoft.com>
2024-05-20 15:42:56 -0700

    remove the correct part

--- SetupDataPkg/Tools/SettingSupport/UefiVariablesSupportLinuxLib.py
+++ SetupDataPkg/Tools/SettingSupport/UefiVariablesSupportLinuxLib.py
@@ -106,7 +106,7 @@ class UefiVariable(object):
             name = name.encode('utf-16')
 
             # NextEntryOffset
-            efi_var_names[offset:(offset + sys.getsizeof(int))] = struct.pack('=I', sys.getsizeof(int) + sys.getsizeof(name) + sys.getsizeof(bytearray(guid)))
+            efi_var_names[offset] = struct.pack('=I', sys.getsizeof(int) + sys.getsizeof(name) + sys.getsizeof(guid))
             offset += sys.getsizeof(int)
 
             # VendorGuid
